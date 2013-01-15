fc3ad096a8c314a0b48b8b9bd00c2c52d1000bb9
Andreas Renberg (IQAndreas) <iq_andreas@hotmail.com>
2013-01-15 17:28:52 +0100

    Save comments as files, and log output

--- commentsubmit.php
+++ commentsubmit.php
@@ -47,6 +47,40 @@ function get_post_data_as_yaml()
 	return $yaml_data;
 }
 
+function log_to($string, $filename = 'mail.log')
+{
+	$log_date = date(DateTime::ATOM);
+	try
+	{
+		$handle = fopen($filename, 'a');
+		fwrite($handle, "[$log_date] $string\n");
+		fclose($handle);
+	}
+	catch (Exception $e) 
+	{
+		// Unable to even log error! You are screwed!
+		//cry();
+	}
+}
+
+function save_as_file($filename, $data)
+{
+	try
+	{
+		$handle = fopen($filename, 'w');
+		fwrite($handle, $data);
+		fclose($handle);
+	}
+	catch (Exception $e) 
+	{
+		log_to("WARNING: Unable to create file '$filename'");
+		return false;
+	}
+	
+	
+	return true;
+}
+
 $COMMENTER_NAME = get_post_field('name', "Anonymous");
 $COMMENTER_EMAIL_ADDRESS = get_post_field('email', $EMAIL_ADDRESS);
 $COMMENTER_WEBSITE = get_post_field('link');
@@ -58,15 +92,42 @@ $POST_ID = get_post_field('post_id', "");
 unset($_POST['post_id']);
 
 
+$yaml_data = "post_id: $POST_ID\n";
+$yaml_data .= "date: $COMMENT_DATE\n";
+$yaml_data .= get_post_data_as_yaml();
+
+$file_date = date('Y-m-d-H-i-s');
+$file_name = Mail::filter_filename($POST_ID, '-') . "-comment-$file_date.yaml";
+
+
+$title = "Comment from $COMMENTER_NAME on '$POST_TITLE'";
+log_to($title);
+
+
 $SPAM = spam_check_text($COMMENT_BODY);
 if (!empty($SPAM))
 {
+	// Save a backup of the file for double checking later for false positives
+	$spam_filename = 'spam' . DIRECTORY_SEPARATOR . $file_name;
+	if (save_as_file($spam_filename, $yaml_data))
+	{
+		log_to("    Suspected SPAM saved as '$spam_filename'");
+	}
+	
 	include $COMMENT_CONTAINS_SPAM;
 	die();
 }
+else
+{
+	$comment_filename = 'comments' . DIRECTORY_SEPARATOR . $file_name;
+	if (save_as_file($comment_filename, $yaml_data))
+	{
+		log_to("    Comments saved as '$comment_filename'");
+	}
+}
 
 
-$subject = "Comment from $COMMENTER_NAME on '$POST_TITLE'";
+$subject = $title;
 
 $message = "$COMMENT_BODY\n\n";
 $message .= "----------------------\n";
@@ -77,22 +138,17 @@ $mail = new Mail($subject, $message);
 $mail->set_from($EMAIL_ADDRESS, $COMMENTER_NAME);
 $mail->set_reply_to($COMMENTER_EMAIL_ADDRESS, $COMMENTER_NAME);
 
-$yaml_data = "post_id: $POST_ID\n";
-$yaml_data .= "date: $COMMENT_DATE\n";
-$yaml_data .= get_post_data_as_yaml();
-
-$attachment_date = date('Y-m-d-H-i-s');
-$attachment_name = Mail::filter_filename($POST_ID, '-') . "-comment-$attachment_date.yaml";
-
 $mail->header_line_ending = $HEADER_LINE_ENDING;
-$mail->set_attachment($yaml_data, $attachment_name);
+$mail->set_attachment($yaml_data, $file_name);
 
 
 if ($mail->send($EMAIL_ADDRESS))
 {
+	log_to("    Sent as email to '$EMAIL_ADDRESS'");
 	include $COMMENT_RECEIVED;
 }
 else
 {
+	log_to("    ERROR: Unable to send email to '$EMAIL_ADDRESS'. Diagnose ASAP!");
 	echo "There was a problem sending the comment. Please contact the site's owner.";
 }
