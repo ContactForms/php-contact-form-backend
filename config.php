960ce7efda5fda5793bc8905e8b563be4977812e
Andreas Renberg (IQAndreas) <iq_andreas@hotmail.com>
2013-01-15 16:45:01 +0100

    Move config to separate PHP file
    This avoids having to modify the variables every time the backend code is updated.

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+
+// Where the comment e-mails should be sent to.  This will also be used as
+// the From: address.  Whilst you could, in theory, change this to take the
+// address out of the form, it's *incredibly* highly recommended you don't,
+// because that turns you into an open relay, and that's not cool.
+$EMAIL_ADDRESS = "blogger@example.com";
+
+// The contents of the following file (relative to this PHP file) will be
+// displayed after the comment is received.  Customise it to your heart's
+// content.
+$COMMENT_RECEIVED = "comment_received.html";
+
+// The contents of the following file (relative to this PHP file) will be
+// displayed if the comment contains spam.  Customise it to your heart's
+// content.
+$COMMENT_CONTAINS_SPAM = "comment_contains_spam.html";
+
+// Format of the date you want to use in your comments.  See
+// http://php.net/manual/en/function.date.php for the insane details of this
+// format.
+$DATE_FORMAT = "Y-m-d H:i";
+
+// If the emails arrive in your client "garbled", you may need to change this
+// line to "\n" instead.
+$HEADER_LINE_ENDING = "\r\n";
+
+
